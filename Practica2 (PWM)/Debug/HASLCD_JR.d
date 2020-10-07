24f81d493c8e232a83b2284ef4b7cd247eee8cdb
Edesalher <edalher117@gmail.com>
2020-10-06 22:09:45 -0600

    Practica 2 terminada.

---
+++ Practica2 (PWM)/Debug/HASLCD_JR.d
@@ -0,0 +1,37 @@
+# FIXED
+
+HASLCD_JR.obj: ../HASLCD_JR.c
+HASLCD_JR.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+HASLCD_JR.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+HASLCD_JR.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+HASLCD_JR.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+HASLCD_JR.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+HASLCD_JR.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+HASLCD_JR.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+HASLCD_JR.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+HASLCD_JR.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+HASLCD_JR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+HASLCD_JR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+HASLCD_JR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+HASLCD_JR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+HASLCD_JR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+HASLCD_JR.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+HASLCD_JR.obj: ../HASLCD_JR.h
+
+../HASLCD_JR.c: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+../HASLCD_JR.h: 
